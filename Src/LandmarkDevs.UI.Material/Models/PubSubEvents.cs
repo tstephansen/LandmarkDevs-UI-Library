859f20bd12bd8bfecaca57995e3264c72ff9fdbf
Tim Stephansen <tim@landmarkdevs.com>
2017-05-28 18:56:37 -0400

    Added pubsubevents.

---
+++ Src/LandmarkDevs.UI.Material/Models/PubSubEvents.cs
@@ -0,0 +1,53 @@
+﻿using Prism.Events;
+using System.Diagnostics.CodeAnalysis;
+
+namespace LandmarkDevs.UI.Material.Models
+{
+    /// <summary>
+    /// Shows a standard dialog to the user.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public sealed class ShowDialogEvent : PubSubEvent<DialogModel>
+    {
+    }
+
+    /// <summary>
+    /// Shows or hides the loading state on the main window.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public sealed class ShellLoadingStateVisible : PubSubEvent<bool>
+    {
+    }
+
+    /// <summary>
+    /// Shows or hides the navigation bar.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public sealed class ChangeNavbarVisibilityEvent : PubSubEvent<bool>
+    {
+    }
+
+    /// <summary>
+    /// Toggles help tooltips throughout the program.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public sealed class ToggleHelpTooltipsEvent : PubSubEvent<bool>
+    {
+    }
+
+    /// <summary>
+    /// Sets the text on the status bar.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public sealed class SetStatusBarTextEvent : PubSubEvent<string>
+    {
+    }
+
+    /// <summary>
+    /// Shows or hides the menu button.
+    /// </summary>
+    [ExcludeFromCodeCoverage]
+    public sealed class SetMenuButtonVisibleEvent : PubSubEvent<bool>
+    {
+    }
+}
---
+++ pack.ps1
@@ -0,0 +1,3 @@
+nuget pack Src/LandmarkDevs.UI.Material/LandmarkDevs.UI.Material.csproj -Build -Properties Configuration=Release
+
+cp Src/LandmarkDevs.UI.Material/bin/Release/LandmarkDevs.UI.Material.pdb LandmarkDevs.UI.Material.pdb
